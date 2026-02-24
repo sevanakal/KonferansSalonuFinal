92df308c43d7f1d320708ffa4aeef265183f0711
Sevan <spidersevan@hotmail.com>
2026-02-24 18:28:37 +0300

    SeatGroup Service update

--- KonferansSalonu/Services/ISeatGroup.cs
+++ KonferansSalonu/Services/ISeatGroup.cs
@@ -1,5 +1,6 @@
 ﻿using KonferansSalonu.Dto;
 using KonferansSalonu.Models;
+using Microsoft.EntityFrameworkCore;
 namespace KonferansSalonu.Services
 {
     public class ISeatGroup : ISeatGroupService
@@ -29,6 +30,52 @@ namespace KonferansSalonu.Services
                 await _context.SaveChangesAsync();
                 return true;
             }
+        }
+
+        public async Task<bool> SaveSeatGroupDesign(int sectionId, List<DesignItem> designItem, List<SeatGroupDto> seatGroupDto) 
+        {
+            using var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
+            {
+                // Eski koltuk gruplarını ve koltukları sil
+                var oldGroups = await _context.Seatgroups.Include(g => g.Seats).Where(g => g.Sectionid == sectionId).ToListAsync();
+
+                var oldSeats = oldGroups.SelectMany(g => g.Seats).ToList();
+
+                _context.Seats.RemoveRange(oldSeats);
+                _context.Seatgroups.RemoveRange(oldGroups);
+                await _context.SaveChangesAsync();
+
+                var newSeatGroups = new List<Seatgroup>();
+                var newSeats = new List<Seat>();
+
+                foreach (var group in seatGroupDto)
+                {
+                    var newGroup = new Seatgroup
+                    {
+                        Sectionid = sectionId,
+                        Name = group.Name,
+                        Color = group.Color
+                    };
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return false;
+        }
+
+        private Seat MaptToSeatEntity(DesignItem designItem, int seatGroupId)
+        {
+            return new Seat
+            {
+                
+                
+                
+            };
         }
     }
 }
--- KonferansSalonu/Services/ISeatGroupService.cs
+++ KonferansSalonu/Services/ISeatGroupService.cs
@@ -5,5 +5,7 @@ namespace KonferansSalonu.Services
     public interface ISeatGroupService
     {
         Task<bool> CreateSeatGroup(SeatGroupDto seatGroup);
+
+        Task<bool> SaveSeatGroupDesign(int sectionId, List<DesignItem> designItem, List<SeatGroupDto> seatGroupDto);
     }
 }
